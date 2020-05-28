24ffba708b5613d271dba60bceecbe9f5076b3f5
Bogdan Kirillov <bakirillov1488@gmail.com>
2020-05-28 23:24:00 +0300

    subparsers, edit and update

--- TEMPLATE.md
+++ TEMPLATE.md
@@ -1,26 +1,34 @@
 # ID: TITLE
 
 ## Contents
-CONTENTS_OF_THE_CARD
-'no more than 5 sentences, short description of an idea'
-'Use [[]] to link two cards together'
+CONTENTS_OF_THE_CARD    
+'no more than 5 sentences, short description of an idea'    
+'Use [[]] to link two cards together'    
 
 ## Source
-SOURCE_OF_THE_CARD
-'Link or book, article etc. May be more than one.'
-'Forms an additional link between two cards'
-
-## Timeline
-TIMELINE_OF_THE_CARD
-'The ids of old versions, this is created using exocat update'
-'Forms an additional link between two cards'
+SOURCE_OF_THE_CARD   
+'Link or book, article etc. May be more than one.'    
+'Forms an additional link between two cards'    
 
 ## Comments
-COMMENTS_FOR_THE_CARD
-'Comments about the contents of the card'
-'Form additional link between two cards'
+COMMENTS_FOR_THE_CARD   
+'Comments about the contents of the card'   
+'Form additional link between two cards'   
 
 ## Method
-COMMENTS_FOR_THE_METHOD_OF_SEARCH
-'Comments about the method of card introduction'
-'Forms additional link between two cards'
+COMMENTS_FOR_THE_METHOD_OF_SEARCH    
+'Comments about the method of card introduction'    
+'Forms additional link between two cards'    
+
+## Study
+### Questions
+* Write questions as follows    
+* In the study mode the random question will be asked    
+### Answers
+* Write corresponding answers here    
+
+## Timeline
+TIMELINE_OF_THE_CARD    
+'The ids of old versions, this is created using exocat update'   
+'Forms an additional link between two cards'    
+
--- exocat.py
+++ exocat.py
@@ -21,7 +21,7 @@ class ExoCat():
         with open("config.json", "r") as ih:
             self.config = json.loads(ih.read())
         
-    def new(self, title):
+    def new(self, title, study_mode, old=[]):
         cid = datetime.now().strftime("%d%m%Y%H%M%S")
         if not op.exists(self.config["folder"]):
             os.makedirs(self.config["folder"])
@@ -30,26 +30,78 @@ class ExoCat():
         if title == "None":
             title = input("Enter the title of the card: ")
         tmpl = tmpl.replace("TITLE", title)
+        spl = tmpl.split("\n")
+        if len(old) == 0:
+            whr = spl.index("## Study" if not study_mode else "## Timeline")
+        else:
+            whr = spl.index("## Timeline") + 1
+        spl = spl[0:whr]
+        tmpl = "\n".join(spl)
+        if len(old) > 0:
+            tmpl += "\n"+"\n".join(old)
         with open(path, "w") as oh:
             oh.write(tmpl)
         os.system(self.config["editor"]+" "+path)
+    
+    def update(self, cid):
+        if cid == "None":
+            cid = input("Enter the id of the card to edit: ")
+        path = op.join(self.config["folder"], cid+".md")
+        with open(path, "r") as ih:
+            old = ih.read()
+        title = old.split("\n")[0].replace("# ", "").split(": ")[-1]
+        self.new(title, False, old = [cid])
         
     def index(self):
         pass
+    
+    def edit(self, cid):
+        if cid == "None":
+            cid = input("Enter the id of the card to edit: ")
+        path = op.join(self.config["folder"], cid+".md")
+        os.system(self.config["editor"]+" "+path)
         
+        
+def new_card(args):
+    cat = ExoCat()
+    cat.new(args.title, args.study)
+    
+def edit_card(args):
+    cat = ExoCat()
+    cat.edit(args.card_id)
+    
+def update_card(args):
+    cat = ExoCat()
+    cat.update(args.card_id)
 
+    
 if __name__ == "__main__":
     print(THECAT)
     parser = argparse.ArgumentParser(description="A personal CLI exocortex assistant")
-    parser.add_argument(
-        "command", action="store", help="What should the ExoCat do?", metavar="command",
-        choices=["new", "index", "update", "search", "move", "study"]
-    )
-    parser.add_argument(
+    subparsers = parser.add_subparsers()
+    parser_new = subparsers.add_parser("new", help="Make a new card")
+    parser_new.add_argument(
         "-t", "--title", help="The title of the card",
         default="None"
     )
+    parser_new.add_argument(
+        "-s", "--study", help="Create the card in study mode",
+        action="store_true"
+    )
+    parser_new.set_defaults(func=new_card)
+    parser_edit = subparsers.add_parser("edit", help="Edit an old card")
+    parser_edit.add_argument(
+        "-c", "--card-id", help="The id of the card to edit",
+        default="None"
+    )
+    parser_edit.set_defaults(func=edit_card)
+    parser_update = subparsers.add_parser(
+        "update", help="Update an old card with additional information"
+    )
+    parser_update.add_argument(
+        "-c", "--card-id", help="The id of the card to edit",
+        default="None"
+    )
+    parser_update.set_defaults(func=update_card)
     args = parser.parse_args()
-    cat = ExoCat()
-    if args.command == "new" or args.command == "n":
-        cat.new(args.title)
+    args.func(args)
